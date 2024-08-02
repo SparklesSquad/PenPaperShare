63350276cdda18dc819936a11d7913064e877ca6
Jaswanth Mada <jaswanth@Jaswanths-Air.ht.home>
2024-08-02 13:40:35 -0500

    Added todo file

---
+++ backend/todo.js
@@ -0,0 +1,7 @@
+// TODO: Update all the responses in the controllers.
+// TODO: Admin Dashboard
+// TODO: Reroder routes in a order
+// TODO: Update naming convention.
+// TODO: Optimize the code
+// TODO: Observe how much storage each document and file are taking in mongodb and s3.
+// TODO: Perform parallel API testing on the backend.
